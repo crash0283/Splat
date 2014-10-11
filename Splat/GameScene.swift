871f6951a97af685f3faae142122fea87864921d
crash0283 <chrisrasch@chriss-mbp.home>
2014-10-11 12:51:40 -0500

    added steering wheel and dash

--- Splat/GameScene.swift
+++ Splat/GameScene.swift
@@ -13,12 +13,14 @@ class GameScene: SKScene {
         /* Setup your scene here */
         
         var bg = SKSpriteNode (imageNamed: "bg.png")
-        bg.position = CGPointMake(self.frame.size.width / 2 + 100, self.frame.size.height / 2)
+        bg.position = CGPointMake(self.frame.size.width / 2, self.frame.size.height / 2 + 50)
         bg.size = CGSizeMake(self.frame.size.width + 400, self.frame.size.height)
+        bg.zPosition = 0
         self.addChild(bg)
         
         var cloud1 = SKSpriteNode (imageNamed: "clouds_01.png")
         cloud1.position = CGPointMake(self.frame.size.width / 2, self.frame.size.height / 2 + 100)
+        cloud1.zPosition = 1
         self.addChild(cloud1)
         
         /*var cloud1move = SKAction.moveByX(10000, y: 0, duration: 10000)
@@ -26,10 +28,25 @@ class GameScene: SKScene {
         
         var cloud2 = SKSpriteNode (imageNamed: "clouds_02.png")
         cloud2.position = CGPointMake(self.frame.size.width / 2 + 200, self.frame.size.height / 2 + 100)
+        cloud2.zPosition = 2
         self.addChild(cloud2)
         
         /*var cloud2move = SKAction.moveByX(-10000, y: 0, duration: 5000)
         cloud2.runAction(cloud2move)*/
+        
+        var dash = SKSpriteNode (imageNamed: "dash.png")
+        dash.position = CGPointMake(self.frame.size.width / 2, self.frame.size.height / 2 - 280)
+        dash.zPosition = 10
+        self.addChild(dash)
+        
+        var steeringWheel = SKSpriteNode (imageNamed: "steeringWheel.png")
+        steeringWheel.position = CGPointMake(self.frame.size.width / 2 - 400, self.frame.size.height / 2 - 260)
+        steeringWheel.zPosition = 20
+        self.addChild(steeringWheel)
+
+        
+
+        
         
         
         

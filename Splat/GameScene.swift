3668475d2b4e2ea384a82b17d4cef90f7bbd02f2
crash0283 <chrisrasch@chriss-mbp.home>
2014-10-11 12:29:49 -0500

    add clouds

--- Splat/GameScene.swift
+++ Splat/GameScene.swift
@@ -17,6 +17,20 @@ class GameScene: SKScene {
         bg.size = CGSizeMake(self.frame.size.width + 400, self.frame.size.height)
         self.addChild(bg)
         
+        var cloud1 = SKSpriteNode (imageNamed: "clouds_01.png")
+        cloud1.position = CGPointMake(self.frame.size.width / 2, self.frame.size.height / 2 + 100)
+        self.addChild(cloud1)
+        
+        /*var cloud1move = SKAction.moveByX(10000, y: 0, duration: 10000)
+        cloud1.runAction(cloud1move)*/
+        
+        var cloud2 = SKSpriteNode (imageNamed: "clouds_02.png")
+        cloud2.position = CGPointMake(self.frame.size.width / 2 + 200, self.frame.size.height / 2 + 100)
+        self.addChild(cloud2)
+        
+        /*var cloud2move = SKAction.moveByX(-10000, y: 0, duration: 5000)
+        cloud2.runAction(cloud2move)*/
+        
         
         
     }
